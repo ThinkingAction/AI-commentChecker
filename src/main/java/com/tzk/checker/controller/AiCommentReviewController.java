122307b77988d26e3fd80be01c995635c4604592
kangkang <mytianzikang@gmail.com>
2026-06-29 23:07:02 +0800

    加入请求参数校验-controller检验注解

--- src/main/java/com/tzk/checker/controller/AiCommentReviewController.java
+++ src/main/java/com/tzk/checker/controller/AiCommentReviewController.java
@@ -3,6 +3,7 @@ package com.tzk.checker.controller;
 import com.tzk.checker.rep.AiCommentReviewResponse;
 import com.tzk.checker.req.AiCommentReviewRequest;
 import com.tzk.checker.services.AiCommentReviewServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.swagger.annotations.Api;
@@ -19,7 +20,7 @@ public class AiCommentReviewController {
 
     @ApiOperation(value = "评论审核", notes = "根据评论内容判断是否违规，并返回审核结果")
     @PostMapping("/check")
-    public AiCommentReviewResponse aiCommentCheck(@RequestBody AiCommentReviewRequest request) {
+    public AiCommentReviewResponse aiCommentCheck(@Valid @RequestBody AiCommentReviewRequest request) {
         return aiCommentReviewServices.checkComment(request.getCommentStr());
     }
 }
