ad65840c79b5356da56ed007ef3a12bb58370dcd
kangkang <mytianzikang@gmail.com>
2026-06-29 21:38:33 +0800

    init

---
+++ src/main/java/com/tzk/checker/AiCommentCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.tzk.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCommentCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCommentCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzk/checker/AiCommentCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzk.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommentCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
